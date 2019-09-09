0926abf8fbfb49395da59988df646f085513b7c0
trannguyen1510 <44703232+trannguyen1510@users.noreply.github.com>
2019-09-09 10:32:13 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,10 +2,22 @@
 
 using namespace std;
 
+int BinarySearch (int a[], int Left, int Right, int Target)
+{
+
+    if (Left <= Right) {
+        int Mid = (Left + Right)/2;
+        if (Target < a[Mid]) return BinarySearch (a, Left, Mid - 1, Target);
+        if (Target > a[Mid]) return BinarySearch (a, Mid + 1, Right, Target);
+        return Mid;
+    }
+    return -1;
+}
+
 void QuickSort (int a[], int Left, int Right)
 {
     int i = Left, j = Right, Mid = (Left + Right)/2;
-    while (i <= j) {
+    if (Left <= Right) {
         while (a[i] < a[Mid]) i++;
         while (a[j] > a[Mid]) j--;
         if (i <= j) {
@@ -20,7 +32,7 @@ void QuickSort (int a[], int Left, int Right)
 
 int main()
 {
-    int n;
+    int n,m;
     cin >> n;
     int *a = new int [n];
     for (int i = 0; i < n; i++) {
@@ -29,10 +41,13 @@ int main()
     for (int i = 0; i < n; i++) {
         cout << a[i] << " " ;
     }
-    cout << "\nAfter Sorted: ";
     QuickSort(a, 0, n-1);
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " " ;
-    }
+    cout << "Find: ";
+    cin >> m;
+    if (BinarySearch(a, 0, n-1, m) == -1) cout << "No match";
+        else {
+            cout << "Matching!";
+            cout << "\n" << m << " is in position "<< BinarySearch(a, 0, n-1, m);
+        }
     return 0;
 }
