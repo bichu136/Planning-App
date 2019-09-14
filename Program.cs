4fc1d086cd885ef3c501b574ba65194df28bfdb8
bichu136 <bicagu123@gmail.com>
2019-09-14 11:28:37 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ namespace Do_An
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Button Btn = new Button();
+
         }
     }
 }
