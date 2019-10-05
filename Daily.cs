e600f816bea2f56ac8a6bd49a7e3a70aacf93dba
bichu136 <bicagu123@gmail.com>
2019-10-05 15:23:17 +0700

    Fix something went wrong

---
+++ Daily.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_An
+{
+	class Daily : ThingsToDo
+	{
+		int factor;
+		public override int getTotalScore(int Minute)
+		{
+			return base.getTotalScore(Minute);
+		}
+		public override void updateStatus()
+		{
+			if (lastupdate /*chỗ này cộng 1 khoảng thời gian*/< DateTime.Now)
+			{
+				Status = -1;
+				return;
+			}
+		}
+	}
+}
---
+++ Jobs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_An
+{
+    class Jobs
+    {
+        public enum Status {Ongoing=1,Dropped=-1,Waiting=2,Complete=0 };
+        public enum Stats { Works = 1, Relax = 0, Sports=2};
+        protected String ID;
+        protected String name;
+        protected int status;
+        protected int stats;
+        protected int Score;
+        protected DateTime lastupdate;
+        void setScoreAndStats(int stats)
+        {
+            this.stats = stats % 10;
+        }
+        void UpdateDrop() {
+
+        }
+    }
+}
