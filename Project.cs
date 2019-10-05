a701ea6ca52072a18b0518f53d34fac3ebfd3118
ocean2000 <daiduong20061905@gmail.com>
2019-10-05 20:30:12 +0700

    Thuc hien xong 1 so class

---
+++ Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_An
+{
+	class Project : ThingsToDo
+	{
+		DateTime Deadline;
+		public override int getTotalScore(int Minute)
+		{
+			return base.getTotalScore(Minute);
+		}
+	}
+}
