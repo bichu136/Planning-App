29d700e9954320b5868884dd95a83a58cc70e6a9
bichu136 <bicagu123@gmail.com>
2019-10-06 21:47:49 +0700

    Fix bichu mess

---
+++ AddThingToDoForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Do_An
+{
+    partial class AddThingToDoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TypeCbBox = new System.Windows.Forms.ComboBox();
+            this.TypeLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TypeCbBox
+            // 
+            this.TypeCbBox.FormattingEnabled = true;
+            this.TypeCbBox.Items.AddRange(new object[] {
+            "Event",
+            "Objective",
+            "Daily",
+            "Project"});
+            this.TypeCbBox.Location = new System.Drawing.Point(58, 12);
+            this.TypeCbBox.Name = "TypeCbBox";
+            this.TypeCbBox.Size = new System.Drawing.Size(241, 24);
+            this.TypeCbBox.TabIndex = 0;
+            // 
+            // TypeLbl
+            // 
+            this.TypeLbl.AutoSize = true;
+            this.TypeLbl.Location = new System.Drawing.Point(12, 15);
+            this.TypeLbl.Name = "TypeLbl";
+            this.TypeLbl.Size = new System.Drawing.Size(40, 17);
+            this.TypeLbl.TabIndex = 1;
+            this.TypeLbl.Text = "Type";
+            this.TypeLbl.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // AddThingToDoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TypeLbl);
+            this.Controls.Add(this.TypeCbBox);
+            this.Name = "AddThingToDoForm";
+            this.Text = "AddThingToDoForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox TypeCbBox;
+        private System.Windows.Forms.Label TypeLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ AddThingToDoForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Do_An
+{
+    public partial class AddThingToDoForm : Form
+    {
+        public AddThingToDoForm()
+        {
+            InitializeComponent();
+            TypeCbBox.DisplayMemberChanged += TypeChange;
+        }
+
+        private void TypeChange(object sender, EventArgs e)
+        {
+            switch (TypeCbBox.SelectedIndex)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
