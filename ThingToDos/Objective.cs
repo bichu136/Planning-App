dda60969f3f1734d9f278b34405cdd5a70eba475
bichu136 <bicagu123@gmail.com>
2019-10-08 15:06:03 +0700

    Update ThingToDo related class

---
+++ ThingToDos/Objective.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_An
+{
+	class Objective : ThingsToDo
+	{
+		int Current;
+		int Goal;
+		public Objective(int c, int g)
+		{
+			Current = c;
+			Goal = g;
+		}
+		public override int getTotalScore(int Minute)
+		{
+			return base.getTotalScore(Minute);
+		}
+		public override void updateStatus()
+		{
+			if (lastupdate /*chỗ này cộng 1 khoảng thời gian*/ < DateTime.Now)
+			{
+				Status = -1;
+				return;
+			}
+			if (Current > Goal)
+			{
+				Status = 0;
+				return;
+			}
+		}
+	}
+}
---
+++ ThingToDos/ThingToDo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_An
+{
+	class ThingsToDo
+	{
+		protected string ID;
+		protected string Name;
+		protected int Stat;
+		protected int Score;
+		protected int Status;
+		protected DateTime lastupdate;
+		/*public ThingsToDo(string id, string name,int stat,int score,DateTime lastup) // Constructor : hàm khởi tạo giá trị mặc định có tham số
+        {
+            ID = id;
+            Name = name;
+            Stat = stat;
+            Score = score;
+            Status = 1;
+            lastupdate = lastup;
+        }*/
+		public void update() // phương thức update
+		{
+			lastupdate = DateTime.Now; // cập nhật lastupdate = thời gian hiện tại
+		}
+		virtual public int getTotalScore(int Minute)
+		{
+			return Minute * Score;
+		}
+		virtual public void updateStatus()
+		{
+
+		}
+	}
+}
